a1b9204d5ffe6365b3ada6f170cde6f75e385187
Jay Iyamah <t-iyamah@live.co.uk>
2023-03-22 16:37:55 -0400

    Resolve conflict

--- src/components/WarehouseTableColumns/WarehouseTableColumns.jsx
+++ src/components/WarehouseTableColumns/WarehouseTableColumns.jsx
@@ -1,31 +1,31 @@
-// import "./WarehouseTableColumns.scss";
-// import sortSvg from "../../assets/icons/sort-24px.svg";
+import "./WarehouseTableColumns.scss";
+import sortSvg from "../../assets/icons/sort-24px.svg";
 
-// const WarehouseTableColumns = () => {
-//   return (
-//     <div className="warehouse__columns">
-//       <h4>
-//         warehouse
-//         <img src={sortSvg} alt="sort" />
-//       </h4>
-//       <h4>
-//         address
-//         <img src={sortSvg} alt="sort" />
-//       </h4>
-//       <h4>
-//         contact name
-//         <img src={sortSvg} alt="sort" />
-//       </h4>
-//       <h4>
-//         contact information
-//         <img src={sortSvg} alt="sort" />
-//       </h4>
-//       <h4>
-//         actions
-//         <img src={sortSvg} alt="sort" />
-//       </h4>
-//     </div>
-//   );
-// };
+const WarehouseTableColumns = () => {
+  return (
+    <div className="warehouse__columns">
+      <h4>
+        warehouse
+        <img src={sortSvg} alt="sort" />
+      </h4>
+      <h4>
+        address
+        <img src={sortSvg} alt="sort" />
+      </h4>
+      <h4>
+        contact name
+        <img src={sortSvg} alt="sort" />
+      </h4>
+      <h4>
+        contact information
+        <img src={sortSvg} alt="sort" />
+      </h4>
+      <h4>
+        actions
+        <img src={sortSvg} alt="sort" />
+      </h4>
+    </div>
+  );
+};
 
-// export default WarehouseTableColumns;
+export default WarehouseTableColumns;
