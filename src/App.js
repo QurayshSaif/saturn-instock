6a074c33ffab97e440a7c39ac6a2e1cc3d4a4d4c
QurayshSaif <saifquraysh@gmaill.com>
2023-03-23 15:19:29 -0400

    deleted component and added it to pages folder

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import './App.scss'
+import "./App.scss";
 import React from "react";
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddWarehouse from './components/AddWarehouse/AddWarehouse';
-import WarehouseList from './components/WarehouseList/WarehouseList';
+import AddWarehouse from "./components/AddWarehouse/AddWarehouse";
+import WarehouseList from "./components/WarehouseList/WarehouseList";
+import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage";
 
-
-
-function App () {
-  return (      
-<BrowserRouter>
-<Header/>
-<Routes>
-<Route path="/" element={<AddWarehouse/>} />
-<Route path="/Warehouse" element={<WarehouseList />} />
-<Route path="Warehouse/add" element={<AddWarehouse />} />
-<Route path="Warehouse/:id" element={<WarehouseList />} />
-<Route path="Warehouse/:id/edit" element={<WarehouseList />} />
-<Route path="Warehouse/:id/delete" element={<WarehouseList />} />
-<Route path="/Inventory" element={<WarehouseList />} />
-<Route path="Inventory/add" element={<WarehouseList />} />
-<Route path="Inventory/:id" element={<WarehouseList />} />
-<Route path="Inventory/:id/edit" element={<WarehouseList />} />
-<Route path="Inventory/:id/delete" element={<WarehouseList />} />
-</Routes>
-<Footer/>
-</BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="warehouses/:id" element={<WarehouseDetailsPage />} />
+        <Route path="/" element={<AddWarehouse />} />
+        <Route path="/Warehouse" element={<WarehouseList />} />
+        <Route path="Warehouse/add" element={<AddWarehouse />} />
+        <Route path="Warehouse/:id" element={<WarehouseList />} />
+        <Route path="Warehouse/:id/edit" element={<WarehouseList />} />
+        <Route path="Warehouse/:id/delete" element={<WarehouseList />} />
+        <Route path="/Inventory" element={<WarehouseList />} />
+        <Route path="Inventory/add" element={<WarehouseList />} />
+        <Route path="Inventory/:id" element={<WarehouseList />} />
+        <Route path="Inventory/:id/edit" element={<WarehouseList />} />
+        <Route path="Inventory/:id/delete" element={<WarehouseList />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
