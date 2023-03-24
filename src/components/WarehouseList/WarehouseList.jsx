0f0a4a62817f1a8c2966284b2c42dd42a643b38a
bennygoldman <goldmanb@gmail.com>
2023-03-23 23:22:16 -0400

    server working

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,26 +1,26 @@
 import "./WarehouseList.scss";
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
 import WarehouseListHeader from "../WarehouseListHeader/WarehouseListHeader";
 import WarehouseTableColumns from "../WarehouseTableColumns/WarehouseTableColumns";
 import WarehouseTableRow from "../WarehouseTableRow/WarehouseTableRow";
-import { API_URL_BASE, WAREHOUSE_PATH } from '../../utils/utils'
+import {API_URL_BASE, WAREHOUSE_PATH} from '../../utils/utils'
 import axios from 'axios'
 
 
 const WarehouseList = () => {
 
-  const [ warehouseList, setWarehouseList ] = useState([])
-  const warehouseUrl = `${ API_URL_BASE }${ WAREHOUSE_PATH }`
-  const { id } = useParams()
+  const {id} = useParams()
+  const [warehouseList, setWarehouseList] = useState([])
+  const warehouseUrl = `${API_URL_BASE}/${WAREHOUSE_PATH}`
 
 
   useEffect(() => {
-    document.title = 'InStock - Warehouses'
+    document.title = 'InStock - Warehouse Overview'
     axios
       .get(warehouseUrl)
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
 
       })
   })
