1717c148039739c1e8649b7fa925292f55f51f0a
Jay Iyamah <t-iyamah@live.co.uk>
2023-03-26 00:04:32 -0400

    updated imports for inventory list

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,7 +1,11 @@
 import "./InventoryList.scss";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import InventoryListHeader from "../InventoryListHeader/InventoryListHeader";
 import InventoryTableColumns from "../InventoryTableColumns/InventoryTableColumns";
 import InventoryTableRow from "../InventoryTableRow/InventoryTableRow";
+import { API_URL_BASE, INVENTORIES_PATH } from "../../utils/utils";
+import axios from "axios";
 
 const InventoryList = () => {
   return (
