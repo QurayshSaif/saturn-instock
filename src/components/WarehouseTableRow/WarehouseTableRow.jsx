981ff46583980affa737b98c7b4f9206ac149f82
bennygoldman <goldmanb@gmail.com>
2023-03-27 23:30:42 -0400

    updates to warehouse list

--- src/components/WarehouseTableRow/WarehouseTableRow.jsx
+++ src/components/WarehouseTableRow/WarehouseTableRow.jsx
@@ -7,44 +7,53 @@ import DeleteModalInventory from "../DeleteModalInventory/DeleteModalInventory";
 
 const WarehouseTableRow = (props) => {
   return (
-    <div className="warehouse__row mobile__container">
-      <div className="mobile__left">
-        <div className="warehouse__block warehouse__name warehouse__mobile warehouse__left">
-          <h4>warehouse</h4>
-          <Link to={`/warehouse/${props.id}`}>
-            {props.warehouseName}
-            <img src={chevronSvg} alt="link" />
-          </Link>
-        </div>
-        <div className="warehouse__block warehouse__address warehouse__left">
-          <h4>address</h4>
-          <p>
-            {props.address}, {props.city}, {props.country}
-          </p>
-        </div>
+    <div className="warehouse__row">
+      {/* <div className="mobile__left"> */}
+      {/* <div className="warehouse__block warehouse__name warehouse__mobile warehouse__left"> */}
+      <div className="warehouse__block warehouse__block--name">
+        <h4>warehouse</h4>
+        <Link to={`/warehouse/${props.id}`}>
+          {props.warehouseName}
+          <img src={chevronSvg} alt="link" />
+        </Link>
       </div>
-      <div className="mobile__right">
-        <div className="warehouse__block warehouse__contact-name">
-          <h4>contact name</h4>
-          <p>{props.name}</p>
-        </div>
-        <div className="warehouse__block warehouse__contact-info">
-          <h4>contact information</h4>
-          <p>{props.phone}</p>
-          <p>{props.email}</p>
-        </div>
+      {/* <div className="warehouse__block warehouse__address warehouse__left"> */}
+      <div className="warehouse__block warehouse__block--address">
+        <h4>address</h4>
+        <p>
+          {props.address}, {props.city}, {props.country}
+        </p>
       </div>
-      <div className="warehouse__block warehouse__block--actions mobile__bottom">
+      {/* </div> */}
+      {/* </div> */}
+      {/* <div className="mobile__right"> */}
+      {/* <div className="warehouse__block warehouse__contact-name"> */}
+      <div className="warehouse__block warehouse__contact-name">
+        <h4>contact name</h4>
+        <p>{props.name}</p>
+      </div>
+      {/* </div> */}
+      {/* <div className="warehouse__block warehouse__contact-info"> */}
+      <div className="warehouse__block warehouse__contact-info">
+        <h4>contact information</h4>
+        <p>{props.phone}</p>
+        <p>{props.email}</p>
+      </div>
+      {/* </div> */}
+      {/* </div> */}
+      {/* <div className="warehouse__block warehouse__block--actions mobile__bottom"> */}
+      <div className="warehouse__block warehouse__block--actions">
         {/* <Link to={`/warehouse/${props.id}/delete`}><img src={deleteSvg} alt="delete" /></Link> */}
         <DeleteModalInventory />
         {/* <Link to={`/warehouse/${props.id}/edit`}><img src={editSvg} alt="edit" /></Link> */}
-        <Link to={`/warehouse/${props.id}/delete`}>
+        {/* <Link to={`/warehouse/${props.id}/delete`}>
           <img src={deleteSvg} alt="delete" />
-        </Link>
+        </Link> */}
         <Link to={`/warehouse/${props.id}/edit`}>
           <img src={editSvg} alt="edit" />
         </Link>
       </div>
+      {/* </div> */}
     </div>
   );
 };
